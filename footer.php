629c05be3cbc511a13ae0da0625112e1bab1f148
Vedmaka <god.vedmaka@gmail.com>
2015-05-24 00:28:18 +0400

    init

---
+++ footer.php
@@ -0,0 +1,49 @@
+
+	<div id="footer" class="footer container-fluid"<?php $this->html( 'userlangattributes' ) ?>>
+    <div class="row">
+	<?php
+      $footerLinks = $this->getFooterLinks();
+
+      if (is_array($footerLinks)) {
+        foreach($footerLinks as $category => $links ):
+          if ($category === 'info') { continue; } ?>
+
+            <ul id="footer-<?php echo $category ?>">
+              <?php foreach( $links as $link ): ?>
+                <li id="footer-<?php echo $category ?>-<?php echo $link ?>"><?php $this->html( $link ) ?></li>
+              <?php endforeach; ?>
+              <?php
+                if ($category === 'places') {
+
+                  # Show sign in link, if not signed in
+                  if ($wgBootstrapSkinLoginLocation == 'footer' && !$this->data['loggedin']) {
+                    $personalTemp = $this->getPersonalTools();
+
+                    if (isset($personalTemp['login'])) {
+                      $loginType = 'login';
+                    } else {
+                      $loginType = 'anonlogin';
+                    }
+
+                    ?><li id="pt-login"><a href="<?php echo $personalTemp[$loginType]['links'][0]['href'] ?>"><?php echo $personalTemp[$loginType]['links'][0]['text']; ?></a></li><?php
+                  }
+
+                  # Show the search in footer to all
+                  if ($wgSearchPlacement['footer']) {
+                    echo '<li>';
+                    $this->renderNavigation( array( 'SEARCHFOOTER' ) ); 
+                    echo '</li>';
+                  }
+                }
+              ?>
+            </ul>
+          <?php 
+              endforeach; 
+            }
+          ?>
+	</div>
+	</div>	
+	
+    <footer>
+
+    </footer>
